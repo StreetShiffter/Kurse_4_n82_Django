97b20468acc33f856c54f32342cbacfe471817c9
StreetShiffter <agwzdushaz@gmail.com>
2025-09-11 16:04:43 +0300

    fix redis url

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,6 @@ LOGIN_URL = 'users:register'# Редирект на страницу регис�
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': os.getenv('REDIS_URL'),
     }
 }
